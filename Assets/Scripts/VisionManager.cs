9eb649e0a11d35c5f9143e5c6ea3df9aea320661
USCCACS02 <usccacs02@cacs01-3.local>
2019-05-30 17:10:31 -0700

    Partially completed get list from server. Completed print list on device

--- Assets/Scripts/VisionManager.cs
+++ Assets/Scripts/VisionManager.cs
@@ -1,108 +1,150 @@
+using Newtonsoft.Json.Linq;
 using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
-using UnityEngine.Networking;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
-[System.Serializable]
-public class TagData
-{
-    public string name;
-    public float confidence;
-}
+public static Vision instance;
 
-[System.Serializable]
-public class AnalysedObject
-{
-    public TagData[] tags;
-    public string requestId;
-    public object metadata;
-}
+// Replace <Subscription Key> with your valid subscription key.
+private string subscriptionKey = "<Subscription Key>";
 
-public static VisionManager instance;
-
-// you must insert your service key here!    
-private string authorizationKey = "- Insert your key here -";    
-private const string ocpApimSubscriptionKeyHeader = "Ocp-Apim-Subscription-Key";
-private string visionAnalysisEndpoint = "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Tags";   // This is where you need to update your endpoint, if you set your location to something other than west-us.
+// You must use the same Azure region in your REST API method as you used to
+// get your subscription keys. For example, if you got your subscription keys
+// from the West US region, replace "westcentralus" in the URL
+// below with "westus".
+//
+// Free trial subscription keys are generated in the "westus" region.
+// If you use a free trial subscription key, you shouldn't need to change
+// this region.
+const string uriBase =
+    "https://westus2.api.cognitive.microsoft.com/vision/v2.0/read/core/asyncBatchAnalyze";
 
 internal byte[] imageBytes;
 
 internal string imagePath;
 
-private void Awake()
-{
-    // allows this instance to behave like a singleton
+private void Awake(){
     instance = this;
 }
 
-/// <summary>
-/// Call the Computer Vision Service to submit the image.
-/// </summary>
-public IEnumerator AnalyseLastImageCaptured()
-{
-    WWWForm webForm = new WWWForm();
-    using (UnityWebRequest unityWebRequest = UnityWebRequest.Post(visionAnalysisEndpoint, webForm))
+public IEnumerator AnalyseLastImageCaptured(){
+    try
     {
-        // gets a byte array out of the saved image
-        imageBytes = GetImageAsByteArray(imagePath);
-        unityWebRequest.SetRequestHeader("Content-Type", "application/octet-stream");
-        unityWebRequest.SetRequestHeader(ocpApimSubscriptionKeyHeader, authorizationKey);
+        HttpClient client = new HttpClient();
 
-        // the download handler will help receiving the analysis from Azure
-        unityWebRequest.downloadHandler = new DownloadHandlerBuffer();
+        // Request headers.
+        client.DefaultRequestHeaders.Add(
+            "Ocp-Apim-Subscription-Key", subscriptionKey);
 
-        // the upload handler will help uploading the byte array with the request
-        unityWebRequest.uploadHandler = new UploadHandlerRaw(imageBytes);
-        unityWebRequest.uploadHandler.contentType = "application/octet-stream";
+        // Assemble the URI for the REST API method.
+        string uri = uriBase;
 
-        yield return unityWebRequest.SendWebRequest();
+        HttpResponseMessage response;
 
-        long responseCode = unityWebRequest.responseCode;     
+        // Two REST API methods are required to extract handwritten text.
+        // One method to submit the image for processing, the other method
+        // to retrieve the text found in the image.
 
-        try
+        // operationLocation stores the URI of the second REST API method,
+        // returned by the first REST API method.
+        string operationLocation;
+
+        // Reads the contents of the specified local image
+        // into a byte array.
+        byte[] byteData = GetImageAsByteArray(imagePath);
+
+        // Adds the byte array as an octet stream to the request body.
+        using (ByteArrayContent content = new ByteArrayContent(byteData))
         {
-            string jsonResponse = null;
-            jsonResponse = unityWebRequest.downloadHandler.text;
+            // This example uses the "application/octet-stream" content type.
+            // The other content types you can use are "application/json"
+            // and "multipart/form-data".
+            content.Headers.ContentType =
+                new MediaTypeHeaderValue("application/octet-stream");
 
-            // The response will be in Json format
-            // therefore it needs to be deserialized into the classes AnalysedObject and TagData
-            AnalysedObject analysedObject = new AnalysedObject();
-            analysedObject = JsonUtility.FromJson<AnalysedObject>(jsonResponse);
-
-            if (analysedObject.tags == null)
-            {
-                Debug.Log("analysedObject.tagData is null");
-            }
-            else
-            {
-                Dictionary<string, float> tagsDictionary = new Dictionary<string, float>();
-
-                foreach (TagData td in analysedObject.tags)
-                {
-                    TagData tag = td as TagData;
-                    tagsDictionary.Add(tag.name, tag.confidence);                            
-                }
-
-                ResultsLabel.instance.SetTagsToLastLabel(tagsDictionary);
-            }
-        }
-        catch (Exception exception)
-        {
-            Debug.Log("Json exception.Message: " + exception.Message);
+            // The first REST API method, Batch Read, starts
+            // the async process to analyze the written text in the image.
+            response = await client.PostAsync(uri, content);
         }
 
-        yield return null;
+        // The response header for the Batch Read method contains the URI
+        // of the second method, Read Operation Result, which
+        // returns the results of the process in the response body.
+        // The Batch Read operation does not return anything in the response body.
+        if (response.IsSuccessStatusCode)
+            operationLocation =
+                response.Headers.GetValues("Operation-Location").FirstOrDefault();
+        else
+        {
+            // Display the JSON error data.
+            string errorString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("\n\nResponse:\n{0}\n",
+                JToken.Parse(errorString).ToString());
+            return;
+        }
+
+        // If the first REST API method completes successfully, the second 
+        // REST API method retrieves the text written in the image.
+        //
+        // Note: The response may not be immediately available. Handwriting
+        // recognition is an asynchronous operation that can take a variable
+        // amount of time depending on the length of the handwritten text.
+        // You may need to wait or retry this operation.
+        //
+        // This example checks once per second for ten seconds.
+        string contentString;
+        int i = 0;
+        do
+        {
+            System.Threading.Thread.Sleep(1000);
+            response = await client.GetAsync(operationLocation);
+            contentString = await response.Content.ReadAsStringAsync();
+            ++i;
+        }
+        while (i < 10 && contentString.IndexOf("\"status\":\"Succeeded\"") == -1);
+
+        if (i == 10 && contentString.IndexOf("\"status\":\"Succeeded\"") == -1)
+        {
+            Console.WriteLine("\nTimeout error.\n");
+            return;
+        }
+        else
+        {   
+             // Display the JSON response.
+            Console.WriteLine("\nResponse:\n\n{0}\n",
+                JToken.Parse(contentString).ToString());
+            
+            List<string> textList = new List<string, float>();
+            
+            foreach(LinesData ld in contentString.recognitionResults.Lines)
+            {
+                textList.Add(ld.text);
+            }
+
+            ResultsLabel.instance.SetTextToLastLabel(textList);
+        }
+                
     }
+    catch (Exception e)
+    {
+        Console.WriteLine("\n" + e.Message);
+    }
+    
+    yield return null;
 }
 
 /// <summary>
 /// Returns the contents of the specified file as a byte array.
 /// </summary>
-private static byte[] GetImageAsByteArray(string imageFilePath)
+private static byte[] GetImageAsByteArray(string imagePath)
 {
-    FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
-    BinaryReader binaryReader = new BinaryReader(fileStream);
+    FileStream fileStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read);
+    BinaryReader BinaryReader = new BinaryReader(fileStream);
     return binaryReader.ReadBytes((int)fileStream.Length);
 }
